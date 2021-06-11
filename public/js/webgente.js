208c690c77d3effb79fde53e2383c29b1204fcd8
Victor Marotta <victordossantosmarotta@gmail.com>
2021-06-11 17:09:30 -0300

    Metadados não aparecem quando caminho especificado não contém public/
    - Por conta da atualização no endereço de salvamento do metadado à partir da 1.2, casos em que o caminho do metadado não vinha com o /public não passavam no split(), logo não aparecendo no link final. Criada condicional para verificação se existe uma string após o /public.

--- public/js/webgente.js
+++ public/js/webgente.js
@@ -82,7 +82,9 @@ Lc = L.control.groupedLayers(baseMaps,overlayMaps,optionsControl).addTo(map);
 function addMetadata (metadata) {
     
     if (metadata != "" && metadata != "none" && metadata != undefined) { // "" é o armazenamento de metadados até a 1.0, a partir da 1.1 o armazenamento sem metadados é denotado como 'none'
-        metadata = metadata.split('public')[1]
+        if (metadata.split('/public')[1] != undefined) {
+            metadata = metadata.split('/public')[1]
+        } 
         return ' <a href="' + metadata + '" target="_blank" style="outline: none;"><i class="fas fa-info-circle"></i></a>'
     } else {
         return ''
